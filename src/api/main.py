6655f6ac14fb669a318926ccde8b174262ec954d
Roberto Furini <robertofurini12@gmail.com>
2025-09-17 11:05:38 -0300

    feat: add main with correct routers

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from .endpoints import health_check
+from .endpoints import health_check, public_router, private_router
 
 app = FastAPI()
 
 app.include_router(health_check.router)
+app.include_router(public_router)
+app.include_router(private_router)
