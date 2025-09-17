566732a2460f9fbb0a9167d233f092b0043eab92
Roberto Furini <robertofurini12@gmail.com>
2025-09-17 11:05:16 -0300

    feat: add MongoDB connection module

---
+++ src/db/mongo.py
@@ -0,0 +1,11 @@
+import os
+from pymongo import MongoClient
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+
+client = MongoClient(MONGO_URL)
+db = client["code_challenge"]
+
+# Coleções
+age_groups_collection = db["age_groups"]
+enrollments_collection = db["enrollments"]
