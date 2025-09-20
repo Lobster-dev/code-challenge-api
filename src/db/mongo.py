6ae38f2e5d084d624651e59735d77e425733e448
Roberto Furini <robertofurini12@gmail.com>
2025-09-20 00:17:49 -0300

    chore(db): make Mongo host configurable and add faster connection check for tests

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -1,9 +1,16 @@
 import os
 from pymongo import MongoClient
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+IS_TESTING = "PYTEST_CURRENT_TEST" in os.environ
+MONGO_HOST = "localhost" if IS_TESTING else os.getenv("MONGO_HOST", "mongo")
+MONGO_URL = f"mongodb://{MONGO_HOST}:27017/"
 
-client = MongoClient(MONGO_URL)
+client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000)
+try:
+    client.admin.command('ismaster')
+except Exception:
+    print("Falha ao conectar ao MongoDB. Verifique se o serviço está rodando.")
+
 db = client["code_challenge"]
 
 age_groups_collection = db["age_groups"]
